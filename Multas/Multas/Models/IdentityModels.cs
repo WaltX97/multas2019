533eea8fe39c4e14e3e539d9eb5f84add8d4af28
Walter Monteiro <aluno20187@ipt.pt>
2019-05-30 15:40:22 +0100

    Concretização da fusao dos dois projetos Sample e Multas

--- Multas/Multas/Models/IdentityModels.cs
+++ Multas/Multas/Models/IdentityModels.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace IdentitySample.Models
+namespace Multas.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -17,11 +18,13 @@ namespace IdentitySample.Models
             return userIdentity;
         }
     }
-
+    /// <summary>
+    /// representa a criaçao da base de dados da autenticação 
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MultasDBConnectionString", throwIfV1Schema: false)
         {
         }
 
@@ -32,9 +35,26 @@ namespace IdentitySample.Models
             Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
         }
 
-        public static ApplicationDbContext Create()
-        {
+        public static ApplicationDbContext Create(){
             return new ApplicationDbContext();
+        }
+
+
+        // vamos colocar, aqui, as instruções relativas às tabelas do 'negócio'
+        // descrever os nomes das tabelas na Base de Dados
+        public virtual DbSet<Multas> Multas { get; set; } // tabela Multas
+        public virtual DbSet<Condutores> Condutores { get; set; } // tabela Condutores
+        public virtual DbSet<Agentes> Agentes { get; set; } // tabela Agentes
+        public virtual DbSet<Viaturas> Viaturas { get; set; } // tabela Viaturas
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+
+
         }
     }
 }
